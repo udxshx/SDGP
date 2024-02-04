563b375ea0af2235a70c2084f324c8beaa67d7c5
Randila Premarathne <randilamenukapremarathne@gmail.com>
2024-02-04 18:46:42 +0530

    Add dotenv and redo the server

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,31 @@
-const express = require("express")
-const app = express()
-const PORT = 8888
-app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`))
+const express = require('express');
+const mongoose = require('mongoose');
+const organizationRoutes = require('/routes/organizationRoutes');
+
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+   console.log(req.path, req.method);
+   next();
+})
+
+//routes
+app.use('/api/organizations', organizationRoutes);
+
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+   console.log('connected to mongodb');
+
+   //listen to port
+   app.listen(process.env.PORT, () => {
+      console.log('Listening for requests on PORT', process.env.PORT)
+   })
+})
+.catch((err) => {
+    console.log(err);
+})
